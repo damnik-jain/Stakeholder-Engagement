abc4b6c0fbdb52ceb72af0cdaf084e11a5d4388c
DamnikJain1 <jaindamnik203@gmail.com>
2018-03-18 18:23:07 +0530

    Changed the dashboard title

--- language/en-gb/common/dashboard.php
+++ language/en-gb/common/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title'] = 'Dashboard';
+$_['heading_title'] = 'Dashboard2';
 
 // Error
 $_['error_install'] = 'Warning: Install folder still exists and should be deleted for security reasons!';
