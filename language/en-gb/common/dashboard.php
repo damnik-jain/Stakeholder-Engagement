9c50a7ba5923bb682d66b765c721dc7221e26682
DamnikJain1 <jaindamnik203@gmail.com>
2018-03-19 09:18:50 +0530

    Demo commit

--- language/en-gb/common/dashboard.php
+++ language/en-gb/common/dashboard.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title'] = 'Dashboard2';
+$_['heading_title'] = 'Dashboard';
 
 // Error
 $_['error_install'] = 'Warning: Install folder still exists and should be deleted for security reasons!';
