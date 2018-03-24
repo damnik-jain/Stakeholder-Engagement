778dd6d5d3608352a7b52b1c59183036aa58597d
DamnikJain1 <jaindamnik203@gmail.com>
2018-03-24 08:25:27 +0530

    Reseted master branch

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8012/SIH/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost:8012/SIH/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8012/SIH/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8012/SIH/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp2/htdocs/SIH/upload/admin/');
+define('DIR_SYSTEM', 'C:/xampp2/htdocs/SIH/upload/system/');
+define('DIR_IMAGE', 'C:/xampp2/htdocs/SIH/upload/image/');
+define('DIR_STORAGE', 'C:/xampp2/storageFinal/');
+define('DIR_CATALOG', 'C:/xampp2/htdocs/SIH/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
