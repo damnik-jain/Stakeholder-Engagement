18737617579108d9b114188dd6fd33e549ee788a
DamnikJain1 <37490624+DamnikJain1@users.noreply.github.com>
2018-03-29 12:03:17 +0530

    Add files via upload

---
+++ language/en-gb/catalog/information_details.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']     = 'Survey Details';
+
+// Text
+$_['text_success']      = 'Success: You have modified the surveys!';
+$_['text_list']         = 'Responses for the selected survey question';
+$_['text_add']          = 'Add Survey';
+$_['text_edit']         = 'Edit Survey';
+$_['text_filter']       = 'Filter';
+
+// Column
+$_['column_question']    = 'Name';
+$_['column_users']     = 'Contact';
+$_['column_rating']     = 'Rating';
+$_['column_status']     = 'Status';
+$_['column_date_added'] = 'Date Added';
+$_['column_action']     = 'Action';
+
+// Entry
+$_['entry_product']     = 'Survey Question';
+$_['entry_author']      = 'Survey Question';
+$_['entry_rating']      = 'Rating';
+$_['entry_status']      = 'Status';
+$_['entry_text']        = 'Text';
+$_['entry_date_added']  = 'Date Added';
+
+// Help
+$_['help_product']      = '(Autocomplete)';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify Surveys!';
+$_['error_product']     = 'Product required!';
+$_['error_author']      = 'Author must be between 3 and 64 characters!';
+$_['error_text']        = 'Review Text must be at least 1 character!';
+$_['error_rating']      = 'Review rating required!';
